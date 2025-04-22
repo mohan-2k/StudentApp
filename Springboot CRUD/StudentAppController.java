8d4c4b0706c62e2ace1181d608c088d0dfc7fa01
mohan-2k <mohanparamasivam92@gmail.com>
2025-04-22 18:58:46 +0530

    Controller - 2

--- Springboot CRUD/StudentAppController.java
+++ Springboot CRUD/StudentAppController.java
@@ -1,20 +1,52 @@
-package com.example.studentapp.controller;
+package com.student.app.studentapp.Controller;
 
-import org.springframework.stereotype.Controller;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
-import com.example.studentapp.Entity.Student;
+import com.student.app.studentapp.Entity.StudentEntity;
+import com.student.app.studentapp.Services.StudentService;
 
-
-@Controller
-public class StudentAppController 
+@RestController
+public class StudentController 
 {
-	@PostMapping("/api/add")
-	public Student addStudent(@RequestBody Student student) {
-		//TODO: process POST request
-		
-		return student;
+	@Autowired
+	StudentService ss;
+	
+	@GetMapping("/getStudents")
+	List<StudentEntity> getAllStudents()
+	{
+		return ss.bringAllStudents();
 	}
 	
+	@GetMapping("/getOne/{id}")
+	StudentEntity getOne(@PathVariable("id") int sid)
+	{
+		return ss.bringOne(sid);
+	}
+	
+	@PutMapping("/insert")
+	String insert(@RequestBody StudentEntity s)
+	{
+		 return ss.save(s);
+	}
+	
+	@DeleteMapping("/delete/{id}")
+	String deleteOne(@PathVariable("id") int sid)
+	{
+		return ss.delete(sid);
+	}
+	
+	@PostMapping("/update/{id}")
+	String updateOne(@PathVariable("id") int sid,@RequestBody StudentEntity se )
+	{
+		return ss.update(sid,se);
+	}
 }
