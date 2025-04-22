d76fe53361fd90ce725c5ed83e86f1facb72b330
mohan-2k <mohanparamasivam92@gmail.com>
2025-04-22 18:56:40 +0530

    Entity or Table Model-1

--- Springboot CRUD/StudentEntity.java
+++ Springboot CRUD/StudentEntity.java
@@ -1,23 +1,48 @@
-package com.example.studentapp.Entity;
+package com.student.app.studentapp.Entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name="student")
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-public class Student 
+public class StudentEntity 
 {
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name="sid")
 	private int id;
+	@Column(name="name")
 	private String name;
+	@Column(name="email")
+	private String email;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public StudentEntity(int id, String name, String email) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+	}
+	public StudentEntity() {
+		super();
+	}
+	
+	
 }
